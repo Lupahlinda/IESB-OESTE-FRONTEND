7652ea88a0308451b6d82f17e91ddf33f1a3a7da
Lupahlinda <Lupahlinda@users.noreply.github.com>
2026-06-08 10:44:28 -0300

    feat: práticas realizadas para a criação do timer pomodoro.

--- projeto-pomodoro/chronos-pomodoro/src/main.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import 'react-toastify/dist/ReactToastify.css'
+
+import './styles/theme.css'
+import './styles/global.css'
+
+import { App } from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
